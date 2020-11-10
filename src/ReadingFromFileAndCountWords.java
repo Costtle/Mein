d421a4003c8483e736b7b64793fed2a22aab073a
Costtle <kosov.eg@outlook.com>
2020-11-10 17:52:40 +0700

    Добавил комментарии

--- src/ReadingFromFileAndCountWords.java
+++ src/ReadingFromFileAndCountWords.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 class ReadingFromFileAndCountWords {
     public static void main(String[] args) throws FileNotFoundException {
         String separator = File.separator;
-        String path = "C:" + separator + "112345.txt";
+        String path = "C:" + separator + "112345.txt"; // можно сделать и полный путь к файлу, либо с сеператором
         File file = new File(path);
 
-        Scanner scanner = new Scanner(file);
-        String line = scanner.nextLine();
-        String[] words = line.split(" ");
-        System.out.println(Arrays.toString(words));
+        Scanner scanner = new Scanner(file); // создаем объект, с которого будет читать файл
+        String line = scanner.nextLine(); // читает строку
+        String[] words = line.split(" "); // в строке делим слова через пробел благодаря line.split
+        System.out.println(Arrays.toString(words)); // делаем вывод слов в массив.
         scanner.close();
         }
 }
