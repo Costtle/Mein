ae56d28ccae883359fe9a82605c41c0e4226d0d2
Costtle <kosov.eg@outlook.com>
2020-11-10 17:36:20 +0700

    Добавил массив, для счета слов в файле

---
+++ src/ReadingFromFileAndCountWords.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+class ReadingFromFileAndCountWords {
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = "C:" + separator + "112345.txt";
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] words = line.split( regex: " ");
+        }
+        scanner.close();
+    }
+}
